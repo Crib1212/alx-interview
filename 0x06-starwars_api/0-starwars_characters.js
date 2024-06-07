881f15c84cf987f002d98f4ab51fd412bda873a7
Crib1212 <igwepatrickchinedu@gmail.com>
2024-06-07 05:49:51 +0100

    pushed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,50 +1,25 @@
 #!/usr/bin/node
-/**
- * Prints all characters of a Star Wars movie
- * The first positional argument passed is the Movie ID
- * Display one character name per line in the same order
- * as  list in the /films/ endpoint
- */
+const request = require("request");
 
-const request = require('request');
-
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error:', response.statusCode, response.statusMessage);
-    process.exit(1);
-  }
-
-  const filmData = JSON.parse(body);
-
-  // Display characters in the same order as the "characters" list in the /films/ endpoint
-  filmData.characters.forEach((characterUrl) => {
-    request(characterUrl, (characterError, characterResponse, characterBody) => {
-      if (characterError) {
-        console.error('Error:', characterError);
-        process.exit(1);
-      }
-
-      if (characterResponse.statusCode !== 200) {
-        console.error('Error:', characterResponse.statusCode, characterResponse.statusMessage);
-        process.exit(1);
-      }
-
-      const characterData = JSON.parse(characterBody);
-      console.log(characterData.name);
-    });
-  });
+const URL = "https://swapi-api.hbtn.io/api/films/";
+request.get(URL + process.argv[2], async (error, response, body) => {
+        if (response.statusCode === 200 && error === null) {
+                const linkChars = JSON.parse(body).characters;
+                const linkCharsList = (chr) => {
+                        const promise = new Promise((resolve, reject) => {
+                                request.get(chr, (error, response, body) => {
+                                        if (error) {
+                                                reject(error);
+                                        } else {
+                                                resolve(body);
+                                        }
+                                });
+                        });
+                        return promise;
+                };
+                for (let i = 0; i < linkChars.length; i++) {
+                        const result = await linkCharsList(linkChars[i]);
+                        console.log(JSON.parse(result).name);
+                }
+        }
 });
